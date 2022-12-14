03eef6d5996a5c8aa5567560878c1c511eee9022
anna <anna@sayf.su>
2022-12-14 10:42:20 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int deposit = 15000;
+        int total = 0;
+        int month = 1;
+        while (total <= 2_459_000) {
+            total = total + deposit;
+            System.out.println("Месяц " + month + ", сумма накомплений равна " + total + " рублей");
+            month ++;
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
     }
 }
